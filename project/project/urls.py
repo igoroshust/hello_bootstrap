55b96b14f747e6225bdb07b121e67439a353ab83
Igor <igoroshust@yandex.ru>
2024-05-27 01:14:41 +0900

    fix navbar, footer, header

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.flatpages import views
+from django.urls import path, include, re_path
 
 urlpatterns = [
+    path('', views.flatpage, {'url': '/main/'}, name='main'),
+    path('pages/', include('django.contrib.flatpages.urls')),
     path('admin/', admin.site.urls),
-    path('pages/', include('django.contrib.flatpages.urls'))
 ]
